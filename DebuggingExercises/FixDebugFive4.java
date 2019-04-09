50819a865d9190b8841acfc5bb47472de5998b6e
edmundpjc <edmundjiancheng.phoon@my.jcu.edu.au>
2019-04-09 13:16:15 +0800

    Syntax errors corrected v2

--- DebuggingExercises/FixDebugFive4.java
+++ DebuggingExercises/FixDebugFive4.java
@@ -16,13 +16,13 @@ public class FixDebugFive4
       three = Integer.parseInt(str);
       str = JOptionPane.showInputDialog(null,"Enter an integer");
       four = Integer.parseInt(str);
-      if(one > two && one >= three && one < four)
+      if(one > two && one > three && one > four)
          output = "Highest is " + one;
       else
          if(two > one || two > three && two > four)
             output = "Highest is " + two;
          else
-           if(three > one && three > two || three == four)
+           if(three > one && three > two || three > four)
               output = "Highest is " + three;
            else
               output = "Highest is " + four;
